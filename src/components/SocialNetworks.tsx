df3c7e82f7eb34545eb0200d57c0bd9bf77ac5b6
mac <neycariom@gmail.com>
2024-06-03 23:36:56 +0100

    links and favicon

--- src/components/SocialNetworks.tsx
+++ src/components/SocialNetworks.tsx
@@ -3,17 +3,23 @@ import { FaLinkedinIn, FaGithub, FaInstagram, FaFacebook } from 'react-icons/fa'
 import '../assets/styles/components/socialnetworks.sass';
 
 const socialNetworks = [
-    { name: 'linkedin', icon: <FaLinkedinIn /> },
-    { name: 'github', icon: <FaGithub /> },
-    { name: 'instagram', icon: <FaInstagram /> },
-    { name: 'facebook', icon: <FaFacebook /> },
+    { name: 'linkedin', icon: <FaLinkedinIn />, link: 'https://www.linkedin.com/in/ney-mac/' },
+    { name: 'github', icon: <FaGithub />, link: 'https://github.com/Ney-Mac' },
+    { name: 'instagram', icon: <FaInstagram />, link: 'https://www.instagram.com/mcariongo/' },
+    { name: 'facebook', icon: <FaFacebook />, link: 'https://www.facebook.com/manuel.acariongo/' },
 ];
 
 export default () => {
     return (
         <section id="social-networks">
             {socialNetworks.map((network, index) => (
-                <a href="#" className='social-btn' id={network.name} key={index}>
+                <a
+                    href={network.link}
+                    className='social-btn'
+                    id={network.name}
+                    key={index}
+                    target='blank'
+                >
                     {network.icon}
                 </a>
             ))}
